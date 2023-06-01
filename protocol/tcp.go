d59ed9d481721b7317c30dfa3d67ce781758eca2
wenyingd <wenyingd@vmware.com>
2023-06-01 16:27:56 +0800

    Bugfix: Support TCP options
    Signed-off-by: wenyingd <wenyingd@vmware.com>

--- protocol/tcp.go
+++ protocol/tcp.go
@@ -1,6 +1,7 @@
 package protocol
 
 import (
+	"antrea.io/libOpenflow/util"
 	"encoding/binary"
 	"errors"
 )
@@ -17,21 +18,27 @@ type TCP struct {
 	WinSize  uint16
 	Checksum uint16
 	UrgFlag  uint16
+	Options  util.Buffer
 
 	Data []byte
 }
 
 func NewTCP() *TCP {
 	u := new(TCP)
+	u.Options = *new(util.Buffer)
 	u.Data = make([]byte, 0)
 	return u
 }
 
 func (t *TCP) Len() (n uint16) {
-	if t.Data != nil {
-		return uint16(20 + len(t.Data))
+	if t.HdrLen < 5 {
+		t.HdrLen = 5
 	}
-	return uint16(20)
+	length := uint16(4 * t.HdrLen)
+	if t.Data != nil {
+		return length + uint16(len(t.Data))
+	}
+	return length
 }
 
 func (t *TCP) MarshalBinary() (data []byte, err error) {
@@ -41,14 +48,22 @@ func (t *TCP) MarshalBinary() (data []byte, err error) {
 	binary.BigEndian.PutUint32(data[4:8], t.SeqNum)
 	binary.BigEndian.PutUint32(data[8:12], t.AckNum)
 
-	data[12] = (t.HdrLen << 4) & 0xf0
+	data[12] = (t.HdrLten << 4) & 0xf0
 	data[13] = t.Code & 0x3f
 
 	binary.BigEndian.PutUint16(data[14:16], t.WinSize)
 	binary.BigEndian.PutUint16(data[16:18], t.Checksum)
 	binary.BigEndian.PutUint16(data[18:20], t.UrgFlag)
 
-	copy(data[20:], t.Data)
+	n := 20
+	var b []byte
+	if b, err = t.Options.MarshalBinary(); err != nil {
+		return
+	}
+	copy(data[n:], b)
+	n += len(b)
+
+	copy(data[n:], t.Data)
 
 	return
 }
@@ -69,11 +84,20 @@ func (t *TCP) UnmarshalBinary(data []byte) error {
 	t.Checksum = binary.BigEndian.Uint16(data[16:18])
 	t.UrgFlag = binary.BigEndian.Uint16(data[18:20])
 
-	if len(data) > 20 {
-		t.Data = make([]byte, (len(data) - 20))
-		copy(t.Data, data[20:])
+	n := 20
+	if t.HdrLen*4 > 20 {
+		t.Options = *new(util.Buffer)
+		err := t.Options.UnmarshalBinary(data[n:int(t.HdrLen*4)])
+		if err != nil {
+			return err
+		}
+		n += int(t.HdrLen*4) - n
+	}
+
+	if len(data) > n {
+		t.Data = make([]byte, (len(data) - int(t.HdrLen*4)))
+		copy(t.Data, data[n:])
 	}
 
 	return nil
-
 }
